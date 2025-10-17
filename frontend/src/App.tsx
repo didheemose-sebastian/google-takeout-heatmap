df506dcd59cd4ae519178113dbf8a9381f3cd9bb
DidheemosePS <sebastiandidheemose2002@gmail.com>
2025-10-17 17:36:57 +0100

    Implemented base map and heatmap layer 	modified:   frontend/package.json 	modified:   frontend/pnpm-lock.yaml 	modified:   frontend/src/App.css 	modified:   frontend/src/App.tsx 	modified:   frontend/src/index.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Map, { useControl } from "react-map-gl/mapbox";
+import { MapboxOverlay } from "@deck.gl/mapbox";
+// import type { MapboxOverlayProps } from "@deck.gl/mapbox";
+import type { DeckProps } from "@deck.gl/core";
+import { HeatmapLayer } from "@deck.gl/aggregation-layers";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// type BikeRack = {
+//   ADDRESS: string;
+//   SPACES: number;
+//   COORDINATES: [longitude: number, latitude: number];
+// };
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function DeckGLOverlay(props: DeckProps) {
+  const overlay = useControl<MapboxOverlay>(() => new MapboxOverlay(props));
+  overlay.setProps(props);
+  return null;
 }
 
-export default App
+export default function App() {
+  const layers = new HeatmapLayer({
+    id: "HeatmapLayer",
+    data: "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json",
+    aggregation: "SUM",
+    getPosition: (d) => d.COORDINATES,
+    getWeight: (d) => d.SPACES,
+    radiusPixels: 25,
+  });
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Map
+        initialViewState={{
+          longitude: -122.45,
+          latitude: 37.75,
+          zoom: 12,
+          pitch: 0,
+          bearing: 0,
+        }}
+        mapStyle="mapbox://styles/mapbox/light-v9"
+        mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+      >
+        <DeckGLOverlay layers={[layers]} />
+      </Map>
+    </div>
+  );
+}
